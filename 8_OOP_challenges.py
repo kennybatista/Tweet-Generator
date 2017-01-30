91f079409bcd02a81bf9a7e6e4fbc8ca80870a39
Kenny Batista <kennybatista7@gmail.com>
2017-01-30 15:00:54 -0800

    challenge 4

--- 8_OOP_challenges.py
+++ 8_OOP_challenges.py
@@ -1,15 +1,38 @@
-def sleep(name):
-    print(name + " sleeps for 8 hours")
+class Tiger(object):
+    def __init__(self, name):
+        self.name = name
+
+    def sleep(self):
+        print(self.name + " sleeps 8 hours")
+
+    def eat(self, food):
+        print(self.name + " eats " + food)
+        favoriteFood = "meat"
+        if favoriteFood == food:
+            print("YUM!" + self.name + " wants more " + food)
 
 
-# Implement the eat function here
-def eat(name, food):
-    print(name + " eats " + food)
-    favoriteFood = "meat"
-    if food == favoriteFood:
-        print("YUM! " + name + " wants more " + food)
+tiger = Tiger("Tiger")
+tiger.eat("meat")
+tiger.sleep()
 
-eat("Leo", "lemons")
-sleep("Leo")
-eat("Tiger", "meat")
-sleep("Tiger")
+
+
+
+class Bear(object):
+    def __init__(self, name):
+        self.name = name
+        self.favoriteFood = "fish"
+
+    def sleep(self):
+        print(self.name + " hibernates for 4 months")
+
+    def eat(self, food):
+        print(self.name + " eats " + food)
+        if food == self.favoriteFood:
+             print("YUM! " + self.name + " wants more " + self.favoriteFood)
+
+
+bear = Bear("Smokey")
+bear.eat("fish")
+bear.sleep()
