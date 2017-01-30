c1a483fe06359513a651cc98fc7bce127fa4e636
Kenny Batista <kennybatista7@gmail.com>
2017-01-30 14:20:23 -0800

    saving status

--- 8_OOP_challenges.py
+++ 8_OOP_challenges.py
@@ -1,15 +1,15 @@
 def sleep(name):
     print(name + " sleeps for 8 hours")
 
-sleep("Rabbit")
 
-
-
+# Implement the eat function here
 def eat(name, food):
     print(name + " eats " + food)
     favoriteFood = "meat"
     if food == favoriteFood:
         print("YUM! " + name + " wants more " + food)
 
-
+eat("Leo", "lemons")
+sleep("Leo")
 eat("Tiger", "meat")
+sleep("Tiger")
