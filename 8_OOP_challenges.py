b5a5e0b0b11736b9230560102a550c48f6a2814e
Kenny Batista <kennybatista7@gmail.com>
2017-01-30 23:46:03 -0800

    challenge 8

--- 8_OOP_challenges.py
+++ 8_OOP_challenges.py
@@ -1,78 +1,46 @@
-# class Tiger(object):
-#     def __init__(self, name):
-#         self.name = name
-#
-#     def sleep(self):
-#         print(self.name + " sleeps 8 hours")
-#
-#     def eat(self, food):
-#         print(self.name + " eats " + food)
-#         favoriteFood = "meat"
-#         if favoriteFood == food:
-#             print("YUM!" + self.name + " wants more " + food)
-#
-#
-# tiger = Tiger("Tiger")
-# tiger.eat("meat")
-# tiger.sleep()
-#
-#
-#
-#
-# class Bear(object):
-#     def __init__(self, name):
-#         self.name = name
-#         self.favoriteFood = "fish"
-#
-#     def sleep(self):
-#         print(self.name + " hibernates for 4 months")
-#
-#     def eat(self, food):
-#         print(self.name + " eats " + food)
-#         if food == self.favoriteFood:
-#              print("YUM! " + self.name + " wants more " + self.favoriteFood)
+# This code tests the Animal, Tiger, Bear, Unicorn, Giraffe and Bee classes
+# and then tests the Zookeeper class and its feedAnimals method
+def test():
+    def getline():
+        # Read a line from standard input and strip surrounding whitespace
+        return sys.stdin.readline().strip()
+    # Get the number of animals
+    animalCount = int(getline())
+    animals = []
+    # Iterate through the input for each animal
+    for count in range(animalCount):
+        # Get the animal's species and name
+        species = getline()
+        name = getline()
+        animal = None
+        # Check what species the animal is
+        if species == "tiger":
+            # Create a Tiger object
+            animal = Tiger(name)
+        elif species == "bear":
+            # Create a Bear object
+            animal = Bear(name)
+        elif species == "unicorn":
+            # Create a Unicorn object
+            animal = Unicorn(name)
+        elif species == "giraffe":
+            # Create a Giraffe object
+            animal = Giraffe(name)
+        elif species == "bee":
+            # Create a Bee object
+            animal = Bee(name)
+        else:
+            # Create an Animal object
+            animal = Animal(name, "kibble")
+        # Add the animal to the list of animals
+        animals.append(animal)
+    # Get the zookeeper's name and food to feed the animals
+    name = getline()
+    food = getline()
+    # Create a Zookeeper object and test its feedAnimals method
+    zookeeper = Zookeeper(name)
+    zookeeper.feedAnimals(animals, food)
 
 
-# bear = Bear("Smokey")
-# bear.eat("fish")
-# bear.sleep()
-
-
-
-class Animal(object):
-    def __init__(self, name):
-        self.name = name
-
-    def sleep(self):
-        print(self.name + " sleeps for 8 hours")
-
-    def eat(self, food):
-        print(self.name + " eats " + food)
-        if food == self.favoriteFood:
-             print("YUM! " + self.name + " wants more " + self.favoriteFood)
-
-
-class Tiger(Animal):
-    def __init__(self, name):
-        self.favoriteFood = "meat"
-        self.name = name
-
-
-
-tiger = Tiger("Lolo")
-tiger.sleep()
-tiger.eat("meat")
-
-class Bear(Animal):
-    def __init__(self, name):
-        self.name = name
-        self.favoriteFood = "fish"
-
-    def sleep(self):
-        print(self.name + " hibernates for 4 months")
-
-
-
-bear = Bear("Benny")
-bear.sleep()
-bear.eat("Blue Berries")
+if __name__ == "__main__":
+    test()
