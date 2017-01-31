cbc2395bb32f245df456b2aa3d2b568da3c04e80
Kenny Batista <kennybatista7@gmail.com>
2017-01-30 21:51:15 -0800

    new challenge

--- 8_OOP_challenges.py
+++ 8_OOP_challenges.py
@@ -1,28 +1,47 @@
-class Tiger(object):
+# class Tiger(object):
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def sleep(self):
+#         print(self.name + " sleeps 8 hours")
+#
+#     def eat(self, food):
+#         print(self.name + " eats " + food)
+#         favoriteFood = "meat"
+#         if favoriteFood == food:
+#             print("YUM!" + self.name + " wants more " + food)
+#
+#
+# tiger = Tiger("Tiger")
+# tiger.eat("meat")
+# tiger.sleep()
+#
+#
+#
+#
+# class Bear(object):
+#     def __init__(self, name):
+#         self.name = name
+#         self.favoriteFood = "fish"
+#
+#     def sleep(self):
+#         print(self.name + " hibernates for 4 months")
+#
+#     def eat(self, food):
+#         print(self.name + " eats " + food)
+#         if food == self.favoriteFood:
+#              print("YUM! " + self.name + " wants more " + self.favoriteFood)
+
+
+# bear = Bear("Smokey")
+# bear.eat("fish")
+# bear.sleep()
+
+
+
+class Animal(object):
     def __init__(self, name):
         self.name = name
-
-    def sleep(self):
-        print(self.name + " sleeps 8 hours")
-
-    def eat(self, food):
-        print(self.name + " eats " + food)
-        favoriteFood = "meat"
-        if favoriteFood == food:
-            print("YUM!" + self.name + " wants more " + food)
-
-
-tiger = Tiger("Tiger")
-tiger.eat("meat")
-tiger.sleep()
-
-
-
-
-class Bear(object):
-    def __init__(self, name):
-        self.name = name
-        self.favoriteFood = "fish"
 
     def sleep(self):
         print(self.name + " hibernates for 4 months")
@@ -33,6 +52,27 @@ class Bear(object):
              print("YUM! " + self.name + " wants more " + self.favoriteFood)
 
 
-bear = Bear("Smokey")
-bear.eat("fish")
+class Tiger(Animal):
+    def __init__(self, name):
+        self.favoriteFood = "meat"
+        self.name = name
+
+
+
+tiger = Tiger("Lolo")
+tiger.sleep()
+tiger.eat("meat")
+
+class Bear(Animal):
+    def __init__(self, name):
+        self.name = name
+        self.favoriteFood = "fish"
+
+    def sleep(self):
+        print(self.name + " hibernates for 4 months")
+
+
+
+bear = Bear("Benny")
 bear.sleep()
+bear.eat("Blue Berries")
